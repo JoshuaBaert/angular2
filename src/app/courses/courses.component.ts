5ca2cd1c2cf13f8e643321347c89ecf24a6ec7c2
Joshua Baert <jbghoring@gmail.com>
2017-06-06 15:08:19 -0500

    More Learning

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -5,9 +5,15 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <h2>Courses</h2>
     {{title}}
+    <ul>
+      <li *ngFor="let course of courses">
+        {{ course }}
+      </li>
+    </ul>
   `,
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
   title = 'The title of the courses page';
+  courses = ['Course 1', 'Course 2', 'Course 3']
 }

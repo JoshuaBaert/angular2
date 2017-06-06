ac667169f29ee779c6330d5cf7e156b1a88cd428
Joshua Baert <jbghoring@gmail.com>
2017-06-06 15:23:13 -0500

    More Learning

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {CoursesService} from './courses.service';
 
 @Component({
   selector: 'app-courses',
-  template: `
-    <h2>Courses</h2>
-    {{title}}
-    <ul>
-      <li *ngFor="let course of courses">
-        {{ course }}
-      </li>
-    </ul>
-  `,
-  styleUrls: ['./courses.component.scss']
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss'],
+  providers: [CoursesService]
 })
 export class CoursesComponent {
   title = 'The title of the courses page';
-  courses = ['Course 1', 'Course 2', 'Course 3']
+  courses;
+
+  constructor(courseService: CoursesService){
+    this.courses = courseService.getCourses();
+  }
 }

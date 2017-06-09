116426cf899c822a0101bfa3ffc252d50bedbaf6
Joshua Baert <jbghoring@gmail.com>
2017-06-09 16:07:21 -0500

    Custom Pipe

---
+++ src/app/pipe/summary.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'summary'
+})
+export class SummaryPipe implements PipeTransform {
+
+  transform(value: string, args) {
+    console.log(args);
+    const limit = (args) ? parseInt(args) : 50;
+
+    if (value) {
+      return value.substring(0, limit) + '...';
+    }
+  }
+
+}

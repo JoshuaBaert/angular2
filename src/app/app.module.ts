2a4058cbd5cc4fc7a5433f2490fef5b3e7ef7f98
Joshua Baert <jbghoring@gmail.com>
2017-06-26 16:24:24 -0500

    Initial router setup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 import 'rxjs/add/operator/map';
 
 
@@ -48,7 +49,17 @@ import { PostComponent } from './post/post.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'courses',
+        component: CoursesComponent
+      },
+      {
+        path: 'like',
+        component: LikeComponent
+      },
+    ])
   ],
   providers: [
     CoursesService,
--- src/app/app.routing.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Joshua Baert on 6/12/2017.
- */
-
-import {Router, RouterModule} from '@angular/router';
-
-@Router({})
